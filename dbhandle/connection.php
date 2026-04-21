79e8e0698fdd54b19edeed7c6db7fe4f63510dcc
Salah-mobile <tabitsalah12@gmail.com>
2026-04-21 11:09:26 +0100

    create the connection with the database

---
+++ dbhandle/connection.php
@@ -0,0 +1,11 @@
+<?php
+function connection(){
+    try {
+     $conn =new PDO("mysql:host=localhost;dbname=edusync", "root", "");
+     return $conn;
+} catch (PDOException $e) {
+    echo "erreur connection";
+    return null;
+}
+
+}
