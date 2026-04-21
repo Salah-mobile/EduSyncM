0f819a01e983c3db35fa50cbf038e3cc37060396
Salah-mobile <tabitsalah12@gmail.com>
2026-04-21 11:26:24 +0100

    create a function that check if the pass and the email correct to give him the acess to a compte

--- formhandle/handlesignin.php
+++ formhandle/handlesignin.php
@@ -1,6 +1,23 @@
 <?php
 include "../dbhandle/connection.php";
 $conn=connection();
+function connectpass($email,$password,$conn){
+    try {
+        $sql ="SELECT * FROM users WHERE users.emil=? and users.password=?";
+        $stm=$conn->prepare($sql);
+        $stm->execute([$email,$password]);
+        $user=$stm->fetch(PDO::FETCH_ASSOC);
+
+         if($user){
+           return true;
+        }else{
+            return false;
+        }
+
+    } catch (PDOException $e) {
+         echo "erreur connect acount"; 
+    }
+}
 if(isset($_POST["signin"])){
     $email=$_POST["email"];
     $password=$_POST["password"];
