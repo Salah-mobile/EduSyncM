ab89a2325beb5cf16b21e128282ba49d193535a8
Anass Jabrane <jabraneanass12@gmail.com>
2026-04-29 15:35:41 +0100

    recuperer l'id de prof

--- pages/prof/dachbordP.php
+++ pages/prof/dachbordP.php
@@ -1,9 +1,13 @@
 <?php
 // pages/prof/dachbordP.php°
+require_once "../../dbhandle/connection.php";
 
 session_start();
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'prof') {
     header("Location: ../../singinpage.php");
     exit;
 }
+
+// 3. Kat-jib l-ID dial l-prof m-la session
+$prof_id = $_SESSION['user']['id'];
 
