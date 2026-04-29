923dd3c3be38557c4bcd77f807c6120882ebfaa4
Anass Jabrane <jabraneanass12@gmail.com>
2026-04-29 11:56:16 +0100

    Contrôle d'accès (Prof uniquement)

--- pages/prof/dachbordP.php
+++ pages/prof/dachbordP.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello Prof</h1>
-</body>
-</html>
+<?php
+// pages/prof/dachbordP.php°
+
+session_start();
+if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'prof') {
+    header("Location: ../../singinpage.php");
+    exit;
+}
+
