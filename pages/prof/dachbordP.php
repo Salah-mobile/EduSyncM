86cf608e01c49fa128ff5a92793981ece4db8ea7
Anass Jabrane <jabraneanass12@gmail.com>
2026-04-30 09:43:40 +0100

    connecter database

--- pages/prof/dachbordP.php
+++ pages/prof/dachbordP.php
@@ -1,13 +1,33 @@
 <?php
-// pages/prof/dachbordP.php°
-require_once "../../dbhandle/connection.php";
 
 session_start();
+function connection(){
+    try {
+     $conn =new PDO("mysql:host=localhost;dbname=edusync", "root", "");
+     return $conn;
+} catch (PDOException $e) {
+    echo "erreur connection";
+    return null;
+}
+}
+
+
+
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'prof') {
     header("Location: ../../singinpage.php");
     exit;
 }
 
-// 3. Kat-jib l-ID dial l-prof m-la session
+
 $prof_id = $_SESSION['user']['id'];
+
+// 4. Requête SQL (US20): Njibo ghir l-cours dial had l-prof
+try {
+    $stmt = $pdo->prepare("SELECT * FROM courses WHERE instructor_id = ?");
+    $stmt->execute([$prof_id]);
+    $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Erreur SQL: " . $e->getMessage());
+}
+?>
 
