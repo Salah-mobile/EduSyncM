cc572f2a9d1c7f5ab1d8b1bbf5984519a80a74b3
Anass Jabrane <jabraneanass12@gmail.com>
2026-04-30 10:15:19 +0100

    cheek url cours

--- pages/prof/course_details.php
+++ pages/prof/course_details.php
@@ -7,3 +7,11 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'Prof') {
     header('Location: login.php');
     exit();
 }
+
+
+// url cours 
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    die("ID du cours manquant.");
+}
+$course_id = $_GET['id'];
+$prof_id = $_SESSION['user_id'];
